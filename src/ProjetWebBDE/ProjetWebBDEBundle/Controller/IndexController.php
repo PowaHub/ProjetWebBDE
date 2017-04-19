c1837d7a9c2f68a057e976c9aef71b7a1d242119
Adam BURGER <adam.burger@viacesi.fr>
2017-04-19 12:25:17 +0200

    Fin maquette
    fin de la maquette tout est pret, let's code

--- src/ProjetWebBDE/ProjetWebBDEBundle/Controller/IndexController.php
+++ src/ProjetWebBDE/ProjetWebBDEBundle/Controller/IndexController.php
@@ -25,10 +25,54 @@ class IndexController extends Controller
     {
         return $this->render('ProjetWebBDEBundle:Default:index.html.twig');
     }
+    public function mentions_legalesAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Default:mentions_legales.html.twig');
+    }
 
     public function photoAction()
     {
         return $this->render('ProjetWebBDEBundle:Default:photo.html.twig');
+    }
+
+    public function activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:activite.html.twig');
+    }
+
+    public function all_activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:all_activite.html.twig');
+    }
+
+    public function ancienne_activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:ancienne_activite.html.twig');
+    }
+
+    public function creation_activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:creation_activite.html.twig');
+    }
+
+    public function nouvelle_activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:nouvelle_activite.html.twig');
+    }
+
+    public function proposer_activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:proposer_activite.html.twig');
+    }
+
+    public function validation_activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:validation_activite.html.twig');
+    }
+
+    public function vote_activiteAction()
+    {
+        return $this->render('ProjetWebBDEBundle:Activites:vote_activite.html.twig');
     }
 
 
