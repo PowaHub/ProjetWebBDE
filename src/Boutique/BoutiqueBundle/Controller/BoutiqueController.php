6232ac3c516e0c28a10d9860109c4aa49e7d2cfb
Adam BURGER <adam.burger@viacesi.fr>
2017-04-19 11:23:13 +0200

    Boutique statique
    on a fini de mettre la boutique statique

--- src/Boutique/BoutiqueBundle/Controller/BoutiqueController.php
+++ src/Boutique/BoutiqueBundle/Controller/BoutiqueController.php
@@ -6,8 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BoutiqueController extends Controller
 {
+	
+
+    public function ajout_articleAction()
+    {
+        return $this->render('BoutiqueBundle:Default:ajout_article.html.twig');
+    }
+
+    public function ajout_categorieAction()
+    {
+        return $this->render('BoutiqueBundle:Default:ajout_categorie.html.twig');
+    }
+
+    public function articleAction()
+    {
+        return $this->render('BoutiqueBundle:Default:article.html.twig');
+    }
+
     public function boutiqueAction()
     {
         return $this->render('BoutiqueBundle:Default:boutique.html.twig');
     }
+
+    public function edition_articleAction()
+    {
+        return $this->render('BoutiqueBundle:Default:edition_article.html.twig');
+    }
+
 }
