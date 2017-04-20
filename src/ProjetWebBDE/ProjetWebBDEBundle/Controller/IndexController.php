12b81fc63d67cd7aab0c45df5e5c2fc4be227d93
Adam BURGER <adam.burger@viacesi.fr>
2017-04-20 16:28:20 +0200

    Contact

--- src/ProjetWebBDE/ProjetWebBDEBundle/Controller/IndexController.php
+++ src/ProjetWebBDE/ProjetWebBDEBundle/Controller/IndexController.php
@@ -3,8 +3,17 @@
 namespace ProjetWebBDE\ProjetWebBDEBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use ProjetWebBDE\ProjetWebBDEBundle\Form\EnquiryType;
+use ProjetWebBDE\ProjetWebBDEBundle\Form\Type\ContactType;
 use ProjetWebBDE\ProjetWebBDEBundle\Entity\Enquiry;
+use ProjetWebBDE\ProjetWebBDEBundle\Form\EnquiryType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\FormEvents;
 
 class IndexController extends Controller
 {
@@ -18,9 +27,35 @@ class IndexController extends Controller
         return $this->render('ProjetWebBDEBundle:Default:bde.html.twig');
     }
 
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('ProjetWebBDEBundle:Default:contact.html.twig');
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+        $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST') {
+            
+            if ($form->isValid() && $form->isSubmitted()) {
+
+                $enquiry=$form->getdata();
+
+                $message = \Swift_Message::newInstance()
+                ->setSubject($enquiry->getSubject())
+                ->setFrom($enquiry->getEmail())
+                ->setTo($this->container->getParameter('projet_web_bde.emails.contact_email'))
+                ->setBody($this->renderView('ProjetWebBDEBundle:Default:contact.txt.twig', array('enquiry' => $enquiry)));
+            $this->get('mailer')->send($message);
+
+            // Redirect - This is important to prevent users re-posting
+            // the form if they refresh the page
+            var_dump("test");
+            return $this->redirectToRoute('contact');
+        }
+    }
+
+    return $this->render('ProjetWebBDEBundle:Default:contact.html.twig', array(
+        'form' => $form->createView()
+    ));
     }
 
     public function indexAction()
