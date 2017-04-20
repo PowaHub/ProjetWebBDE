c338659241f0e2059cadcd6c78b94cc936c96bc2
NappyBoyxX <mehdi.elolmi@viacesi.fr>
2017-04-20 08:59:30 +0200

    Gestion des membres

--- src/User/UserBundle/Controller/roleUsersController.php
+++ src/User/UserBundle/Controller/roleUsersController.php
@@ -15,21 +15,59 @@ class roleUsersController extends Controller
 
 
     /**
-     * @Route("/users/", name="list_users")
-     */
-
-    /**
 	 * @param string $role
 	 *
 	 * @return array
 	 */
     public function listUsersAction(){
 	 
-	   $userManager = $this->get('fos_user.user_manager');
+	    $userManager = $this->get('fos_user.user_manager');
         $users = $userManager->findUsers();
         return $this->render('UserBundle:Default:listUsers.html.twig', array(
             'users' => $users,
         ));
     }
-}
 
+    public function editUsersBDEAction($id)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id' => $id));
+
+        $user-> addRole('ROLE_BDE');
+        $user-> removeRole('ROLE_CESI');
+        $user-> removeRole('ROLE_ETUDIANT');
+        $userManager->updateUser($user); // Pas besoin de faire un flush avec l'EntityManager, cette méthode le fait toute seule !
+        
+        
+        return $this->redirectToRoute("user_role");
+    }
+
+    public function editUsersCESIAction($id)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id' => $id));
+
+        $user-> addRole('ROLE_CESI');
+        $user-> removeRole('ROLE_BDE');
+        $user-> removeRole('ROLE_ETUDIANT');
+        $userManager->updateUser($user); // Pas besoin de faire un flush avec l'EntityManager, cette méthode le fait toute seule !
+        
+        
+        return $this->redirectToRoute("user_role");
+    }
+
+    public function editUsersETUDIANTAction($id)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id' => $id));
+
+        $user-> addRole('ROLE_ETUDIANT');
+        $user-> removeRole('ROLE_CESI');
+        $user-> removeRole('ROLE_BDE');
+        $userManager->updateUser($user); // Pas besoin de faire un flush avec l'EntityManager, cette méthode le fait toute seule !
+        
+        
+        return $this->redirectToRoute("user_role");
+    }
+
+}
---
+++ src/User/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace User\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class RegistrationType extends AbstractType
+{
+
+	public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('prenom')
+            ->add('nom')
+            ->add('adresse')
+            ->add('numero')
+            ->add('file','file',array('required' => false)) 
+        ;
+    }
+
+	public function getParent()
+	{
+		return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+	}
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
+
+ ?>
