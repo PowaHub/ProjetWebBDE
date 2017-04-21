4fedac793eff1b447afbc779507ad6a92091997d
NappyBoyxX <mehdi.elolmi@viacesi.fr>
2017-04-21 12:10:39 +0200

    Edit article

--- src/Boutique/BoutiqueBundle/Controller/ArticleController.php
+++ src/Boutique/BoutiqueBundle/Controller/ArticleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends Controller
 {
-	public function addArticleAction(Request $request)
+    public function addArticleAction(Request $request)
     {
         $article = new Article();
 
@@ -29,7 +29,7 @@ class ArticleController extends Controller
 
             $this->addFlash(
                     'success',
-                    'Vous article a bien été ajouté !'
+                    'Votre article a bien été ajouté !'
             );
 
         }
@@ -45,6 +45,65 @@ class ArticleController extends Controller
             ));
     }
 
+
+
+
+    public function listArticleAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('BoutiqueBundle:Article');
+
+        $articles = $repository->findAll();
+
+
+
+
+        return $this->render('BoutiqueBundle:Default:edition_article.html.twig', array(
+                'articles'=>$articles,
+
+        ));
+
+
+    }
+
+
+    public function editArticleAction(Request $request, Article $article)
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->flush();
+
+            $this->addFlash(
+                    'success',
+                    'Votre article a bien été modifié !'
+            );
+
+        }
+
+
+        $formView = $form->createView();
+
+
+        
+        return $this->render('BoutiqueBundle:Default:ajout_article.html.twig', array(
+                'form'=>$formView,
+
+        ));
+
+
+    }
+
+
+
+
+
     public function deleteArticleAction(Article $article)
     {
         $em = $this->getDoctrine()->getManager();
@@ -55,7 +114,7 @@ class ArticleController extends Controller
 
             $this->addFlash(
                     'success',
-                    'Vous article a bien été supprimé !'
+                    'Votre article a bien été supprimé !'
             );
 
         return $this->redirectToRoute("boutique");
